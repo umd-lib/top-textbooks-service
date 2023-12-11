7f10e3b90e93a980bd9919f14430e0d0d31c8a00
Marc Andreu Grillo Aguilar <aguilarm@umd.edu>
2023-12-11 13:45:12 -0500

    LIBITD-2455: Using python-json-logger for formatting
    Adding the python-json-logger module and replacing previous class
    
    https://umd-dit.atlassian.net/browse/LIBITD-2455

--- src/textbooks/core/utils.py
+++ src/textbooks/core/utils.py
@@ -1,75 +1,24 @@
-import json
-import logging
-from typing import Any, Optional
+from pythonjsonlogger import jsonlogger
 
 
-# Source: https://stackoverflow.com/questions/50144628/python-logging-into-file-as-a-dictionary-or-json
-class JsonFormatter(logging.Formatter):
+class CustomJsonFormatter(jsonlogger.JsonFormatter):
     """
-    Formatter that outputs JSON strings after parsing the LogRecord.
-
-    @param dict fmt_dict: Key: logging format attribute pairs. Defaults to {"message": "message"}.
-    @param str time_format: time.strftime() format string. Default: "%Y-%m-%dT%H:%M:%S"
-    @param str msec_format: Microsecond formatting. Appended at the end. Default: "%s.%03dZ"
+    As long as https://github.com/madzak/python-json-logger/issues/171 is still open,
+    we need to have our own JsonFormatter implementation that fixes the bug.
     """
-    def __init__(self, fmt_dict: Optional[dict[str, Any]] = None,
-                 time_format: str = "%Y-%m-%dT%H:%M:%S",
-                 msec_format: str = "%s.%03dZ"):
-        self.fmt_dict = fmt_dict if fmt_dict is not None else {"message": "message"}
-        self.default_time_format = time_format
-        self.default_msec_format = msec_format
-        self.datefmt = None
 
-    def usesTime(self) -> bool:
-        """
-        Overwritten to look for the attribute in the format dict values instead of the fmt string.
-        """
-        return "asctime" in self.fmt_dict.values()
+    def _perform_rename_log_fields(self, log_record):
+        for old_field_name, new_field_name in self.rename_fields.items():
+            if old_field_name not in log_record:
+                continue
+            log_record[new_field_name] = log_record[old_field_name]
+            del log_record[old_field_name]
 
-    def formatMessage(self, record) -> dict:
-        """
-        Overwritten to return a dictionary of the relevant LogRecord attributes instead of a string.
-        KeyError is raised if an unknown attribute is provided in the fmt_dict.
-        """
-        return {fmt_key: record.__dict__[fmt_val] for fmt_key, fmt_val in self.fmt_dict.items()}
+reserved_attrs = ['taskName', 'args', 'levelno', 'pathname', 'name', 'msg', 'module', 'exc_info', 'exc_text',
+                  'stack_info', 'created', 'relativeCreated', 'msecs', 'thread', 'threadName', 'processName', 'process']
 
-    def format(self, record) -> str:
-        """
-        Mostly the same as the parent's class method, the difference being that a dict is manipulated
-        and dumped as JSON instead of a string.
-        """
-        record.message = record.getMessage()
-
-        if self.usesTime():
-            record.asctime = self.formatTime(record, self.datefmt)
-
-        message_dict = self.formatMessage(record)
-
-        if record.exc_info:
-            # Cache the traceback text to avoid converting it multiple times
-            # (it's constant anyway)
-            if not record.exc_text:
-                record.exc_text = self.formatException(record.exc_info)
-
-        if record.exc_text:
-            message_dict["exc_info"] = record.exc_text
-
-        if record.stack_info:
-            message_dict["stack_info"] = self.formatStack(record.stack_info)
-
-        return json.dumps(message_dict, default=str)
-
-json_formatter = JsonFormatter({"level": "levelname",
-                                    "message": "message",
-                                    "loggerName": "name",
-                                    "processName": "processName",
-                                    "processID": "process",
-                                    "threadName": "threadName",
-                                    "threadID": "thread",
-                                    "timestamp": "asctime"})
-
-logger = logging.getLogger(__name__)
-logHandler = logging.StreamHandler()
-logHandler.setFormatter(json_formatter)
-logger.addHandler(logHandler)
-
+json_formatter = CustomJsonFormatter(json_indent=4,
+                                     timestamp=True,
+                                     reserved_attrs=reserved_attrs,
+                                     rename_fields = { "levelname" : "level" },
+                                    )
