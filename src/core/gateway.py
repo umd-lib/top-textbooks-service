72a0be3c5dca40c07214020cf4bb31d318e48404
David P. Steelman <dsteelma@umd.edu>
2023-12-13 08:43:06 -0500

    LIBITD-2455. Added request response timing to HttpGateway
    Added request response time for HttpGateway requests to the log
    messages.
    
    Intention is to provide insight into how long individual API requests
    are taking, which may be useful for performance tuning.
    
    https://umd-dit.atlassian.net/browse/LIBITD-2455

--- src/core/gateway.py
+++ src/core/gateway.py
@@ -1,3 +1,5 @@
+from time import perf_counter
+
 import requests
 from flask import abort
 
@@ -8,21 +10,22 @@ logger = create_logger(__name__)
 
 class HttpGateway:
     def get(url, params):
-        logger.debug(f'{params=}')
-        logger.debug(f'{url=}')
+        logger.debug(f'{url=}, {params=}')
 
+        request_start_time = perf_counter()
         r = requests.get(url, params=params)
+        request_response_time = f"{(perf_counter() - request_start_time):.4f} seconds"
 
         if r.status_code == 400:
-            abort(r.status_code, f"Received 400 from upstream server '{url}'")
+            abort(r.status_code, f"Received 400 from '{url}' in {request_response_time}")
 
         if r.status_code == 500:
-            abort(r.status_code, f"Received 500 from upstream server '{url}'")
+            abort(r.status_code, f"Received 500 from '{url}' in {request_response_time}")
 
         if not r.ok:
-            logger.warning(f"Received unexpected {r.status_code} from upstream server '{url}'")
+            logger.warning(f"Received unexpected {r.status_code} from '{url}' in {request_response_time}")
             abort(r.status_code, r.reason)
 
-        logger.info(f"Received {r.status_code} from upstream server '{url}'")
+        logger.info(f"Received {r.status_code} from '{url}' in {request_response_time}")
 
         return r.content
