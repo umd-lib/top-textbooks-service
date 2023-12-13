04d0127312c939c86e80c53899dbe5b21abc8274
David P. Steelman <dsteelma@umd.edu>
2023-12-13 08:02:27 -0500

    LIBITD-2455. Added logging filter to redact API keys
    Added "RedactingFilter" class that extends "logging.Filter" to redact
    the ALMA_API key from log messages.
    
    https://umd-dit.atlassian.net/browse/LIBITD-2455

--- src/core/utils.py
+++ src/core/utils.py
@@ -4,22 +4,6 @@ from os import environ
 from dotenv import load_dotenv
 from pythonjsonlogger import jsonlogger
 
-# Determine if DEBUG logging should be enabled
-load_dotenv()
-debug = environ.get("FLASK_DEBUG", default=False)
-
-
-def create_logger(name):
-    """
-    Returns a logger with the given name
-    """
-    logger = logging.getLogger(name)
-    logHandler = logging.StreamHandler()
-    logHandler.setFormatter(json_formatter)
-    logger.addHandler(logHandler)
-    logger.setLevel(logging.DEBUG if debug else logging.INFO)
-    return logger
-
 
 class CustomJsonFormatter(jsonlogger.JsonFormatter):
     """
@@ -35,6 +19,46 @@ class CustomJsonFormatter(jsonlogger.JsonFormatter):
             del log_record[old_field_name]
 
 
+class RedactingFilter(logging.Filter):
+    """
+    Logging Filter implementation that replaces specific strings with
+    "[REDACTED]".
+
+    Primarily intended to prevent API keys from appearing in log messages.
+
+    This filter redacts based on exact string matches (*not* regular
+    expressions).
+
+    Inspired (and largely taken from
+    https://relaxdiego.com/2014/07/logging-in-python.html)
+    """
+    def __init__(self, patterns):
+        super(RedactingFilter, self).__init__()
+        self._patterns = patterns
+
+    def filter(self, record):
+        record.msg = self.redact(record.msg)
+        if isinstance(record.args, dict):
+            for k in record.args.keys():
+                record.args[k] = self.redact(record.args[k])
+        else:
+            record.args = tuple(self.redact(arg) for arg in record.args)
+        return True
+
+    def redact(self, msg):
+        msg = isinstance(msg, str) and msg or str(msg)
+        for pattern in self._patterns:
+            msg = msg.replace(pattern, "'apikey': [REDACTED]")
+        return msg
+
+
+load_dotenv()
+# Determine if DEBUG logging should be enabled
+debug = environ.get("FLASK_DEBUG", default=False)
+# Retrieve API key for redaction
+api_key = environ.get('ALMA_API_KEY', '')
+redacting_filter = RedactingFilter([api_key])
+
 reserved_attrs = [
     'taskName', 'args', 'levelno', 'pathname', 'name', 'msg', 'module',
     'exc_info', 'exc_text', 'stack_info', 'created', 'relativeCreated', 'msecs',
@@ -47,3 +71,16 @@ json_formatter = CustomJsonFormatter(
     reserved_attrs=reserved_attrs,
     rename_fields={'levelname': 'level'},
 )
+
+
+def create_logger(name):
+    """
+    Returns a logger with the given name
+    """
+    logger = logging.getLogger(name)
+    logHandler = logging.StreamHandler()
+    logHandler.setFormatter(json_formatter)
+    logger.addHandler(logHandler)
+    logHandler.addFilter(redacting_filter)
+    logger.setLevel(logging.DEBUG if debug else logging.INFO)
+    return logger
