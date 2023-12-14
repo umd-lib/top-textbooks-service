a5f52d5d668dc39eadfacd36c728ac2aa28396dc
Marc Andreu Grillo Aguilar <aguilarm@umd.edu>
2023-12-14 17:47:02 -0500

    LIBITD-2455: Updating gateway
    Updated the get method to be static, since that's how it's being called,
    and also updating the class to log the error content that would be
    returned by the Alma API, which involved adding an additional method
    
    https://umd-dit.atlassian.net/browse/LIBITD-2455

--- src/core/gateway.py
+++ src/core/gateway.py
@@ -1,6 +1,7 @@
 from time import perf_counter
 
 import requests
+from bs4 import BeautifulSoup
 from flask import abort
 
 from core.logging import create_logger
@@ -9,12 +10,45 @@ logger = create_logger(__name__)
 
 
 class HttpGateway:
+    @staticmethod
+    def _parse_error(content):
+        if content is None:
+            logger.warning('Failed to retreive xml from Alma API')
+            return
+
+        soup = BeautifulSoup(content, features='xml')
+        errors = soup.find_all('error')
+
+        if len(errors) == 0:
+            logger.warning('Failed to find errors in content')
+            return
+
+        for error in errors:
+            try:
+                error_code = error.find('errorCode').text
+                error_message = error.find('errorMessage').text
+            except AttributeError:
+                logger.warning('Failed to retrieve error code and message in content')
+                return
+
+            logger.warning(f'Alma API error {error_code}: {error_message}')
+
+    @staticmethod
     def get(url, params):
         logger.debug(f'{url=}, {params=}')
 
         request_start_time = perf_counter()
         r = requests.get(url, params=params)
         request_response_time = f"{(perf_counter() - request_start_time):.4f} seconds"
+
+        if r.status_code == 400:
+            logger.warning(f"Received {r.status_code} from '{url}' in {request_response_time}")
+            HttpGateway._parse_error(r.content)
+            abort(r.status_code, 'Received 400 from the Alma API')
+
+        if r.status_code == 500:
+            logger.warning(f"Received {r.status_code} from '{url}' in {request_response_time}")
+            abort(r.status_code, 'Received 500 from the Alma API')
 
         if not r.ok:
             logger.warning(f"Received unexpected {r.status_code} from '{url}' in {request_response_time}")
